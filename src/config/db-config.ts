253b9526418ef60caaedbf05015fbdb448123e38
kiritoEM <johankirito64@gmail.com>
2024-01-24 14:29:07 +0100

    add app config

---
+++ src/config/db-config.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ConnectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+
+    if (!process.env.MONGODB_COMPASS_URI) {
+      throw new Error("MongoDB URI is not defined");
+    }
+
+    mongoose.connect(process.env.MONGODB_COMPASS_URI);
+
+    console.log("Connexion réussie avec MongoDB ...");
+  } catch (err) {
+    console.log(err);
+    console.log("connexion failed with Database");
+    process.exit();
+  }
+};
+
+export default ConnectDB;
---
+++ src/server/app.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import ConnectDB from "../config/db-config";
+import cors from "cors";
+import compression from "compression";
+import morgan from "morgan";
+
+dotenv.config();
+const app: Application = express();
+
+//middalware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(compression());
+app.use(morgan("dev"));
+
+//connexion avec mongoDB
+ConnectDB();
+
+app.get("/home", (req: Request, res: Response) => {
+  res.send("Serveur working well!");
+});
+
+// App routes
+// app.use("/pokemon", pokemonRouter);
+
+export default app;
